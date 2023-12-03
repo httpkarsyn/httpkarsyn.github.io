89494969621dec13b07e4a578504848b2c8d036d
httpkarsyn <146144288+httpkarsyn@users.noreply.github.com>
2023-12-02 20:44:24 -0500

    update

--- entries/test/script.js
+++ entries/test/script.js
@@ -1,21 +1,2 @@
-document.addEventListener("DOMContentLoaded", function () {
-      let textOne = document.getElementById("one");
-      let textTwo = document.getElementById("two");
-      let body = document.body;
-
-      function toggleText() {
-        textOne.classList.toggle("hidden-text");
-        textTwo.classList.toggle("hidden-text");
-
-        // Toggle background images based on the visible text
-        if (textOne.classList.contains("hidden-text")) {
-          body.style.backgroundImage = 'none';
-        } else {
-          body.style.backgroundImage = 'none';
-        }
-      }
-
-      // Attach toggleText to the read-again-text click event
-      let readAgainText = document.querySelector(".read-again-text");
-      readAgainText.addEventListener("click", toggleText);
-    });
+var tl = gsap.timeline({ repeat: -1 });
+tl.to("h1", 30, { backgroundPosition: "-960px 0" });
